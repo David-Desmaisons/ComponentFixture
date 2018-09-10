dded57ae8474cb75d50d2127378752e637e02e1b
desmaisons_david <desmaisons_david@hotmail.com>
2018-09-10 19:14:31 -0300

    Adding tests

--- tests/unit/ComponentFixture.spec.js
+++ tests/unit/ComponentFixture.spec.js
@@ -1,12 +1,40 @@
 import { shallowMount } from "@vue/test-utils";
 import ComponentFixture from "@/components/ComponentFixture.vue";
 
+const originalError = console.error;
+
 describe("ComponentFixture.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(ComponentFixture, {
-      propsData: { msg }
+  beforeEach(() => {
+    console.error = () => { };
+  });
+
+  afterEach(() => {
+    console.error = originalError;
+  });
+
+  it("throws when no default slot is passed", () => {
+    const render = () => shallowMount(ComponentFixture, {});
+    expect(render).toThrow("ComponentFixture should have one unique default slot");
+  });
+
+  test.each([['<div/>', '<div/>'], ['<div/>', '<component1/>', '<component2/>']])(
+    'throws when more than one default slots are passed',
+    (...args) => {
+      const render = () => shallowMount(ComponentFixture, {
+        slots: {
+          default: args
+        }
+      });
+      expect(render).toThrow("ComponentFixture should have one unique default slot");
+    },
+  );
+
+  it("does not throw when one default slot is passed", () => {
+    const render = () => shallowMount(ComponentFixture, {
+      slots: {
+        default: '<component/>'
+      }
     });
-    expect(wrapper.text()).toMatch(msg);
+    expect(render).not.toThrow("ComponentFixture should have one unique default slot");
   });
 });
