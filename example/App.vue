5ffd29a5e9ff3951931091ff6e157195d666bea2
David <desmaisons_david@hotmail.com>
2019-03-18 01:26:19 -0300

    Adding svg gauge example

--- example/App.vue
+++ example/App.vue
@@ -1,73 +1,179 @@
 <template>
   <div id="app">
 
-    <ul class="nav nav-tabs" role="tablist">
+    <ul
+      class="nav nav-tabs"
+      role="tablist"
+    >
       <li class="nav-item">
-        <a class="nav-link active" data-toggle="tab" href="#vue-slider-tab" role="tab" aria-controls="profile" aria-selected="true">Vue Slider Component</a>
+        <a
+          class="nav-link active"
+          data-toggle="tab"
+          href="#vue-slider-tab"
+          role="tab"
+          aria-controls="profile"
+          aria-selected="true"
+        >Vue Slider Component</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" data-toggle="tab" href="#sunburst-tab" role="tab" aria-controls="home" aria-selected="false">Sunburst</a>
+        <a
+          class="nav-link"
+          data-toggle="tab"
+          href="#sunburst-tab"
+          role="tab"
+          aria-controls="home"
+          aria-selected="false"
+        >Sunburst</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" data-toggle="tab" href="#tree-tab" role="tab" aria-controls="profile" aria-selected="false">D3tree</a>
+        <a
+          class="nav-link"
+          data-toggle="tab"
+          href="#tree-tab"
+          role="tab"
+          aria-controls="profile"
+          aria-selected="false"
+        >D3tree</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" data-toggle="tab" href="#hello-world-tab" role="tab" aria-controls="profile" aria-selected="false">Hello World</a>
+        <a
+          class="nav-link"
+          data-toggle="tab"
+          href="#hello-world-tab"
+          role="tab"
+          aria-controls="profile"
+          aria-selected="false"
+        >Hello World</a>
       </li>
-      
-    </ul>
-    <div class="tab-content" id="myTabContent">
+      <li class="nav-item">
+        <a
+          class="nav-link"
+          data-toggle="tab"
+          href="#gauge"
+          role="tab"
+          aria-controls="profile"
+          aria-selected="false"
+        >Gauge</a>
+      </li>
 
-      <div class="tab-pane show active" id="vue-slider-tab" role="tabpanel" aria-labelledby="profile-tab">
+    </ul>
+    <div
+      class="tab-content"
+      id="myTabContent"
+    >
+
+      <div
+        class="tab-pane show active"
+        id="vue-slider-tab"
+        role="tabpanel"
+        aria-labelledby="profile-tab"
+      >
         <component-fixture :defaults="{value: 45}">
 
           <!-- Use the default slot to create the component under test -->
           <vue-slider>
-            <div class="diy-tooltip" slot="tooltip" slot-scope="{ value }">
-            {{ value }}
+            <div
+              class="diy-tooltip"
+              slot="tooltip"
+              slot-scope="{ value }"
+            >
+              {{ value }}
             </div>
           </vue-slider>
 
           <!-- Use this slot to enable edition of props values -->
-          <Editor slot="control" slot-scope="scope" v-bind="scope">
+          <Editor
+            slot="control"
+            slot-scope="scope"
+            v-bind="scope"
+          >
           </Editor>
 
         </component-fixture>
       </div>
 
-      <div class="tab-pane fade" id="sunburst-tab" role="tabpanel" aria-labelledby="home-tab">
+      <div
+        class="tab-pane fade"
+        id="sunburst-tab"
+        role="tabpanel"
+        aria-labelledby="home-tab"
+      >
         <component-fixture :defaults="{data: {size:220, children:[{size:45}]}}">
 
           <!-- Use the default slot to create the component under test -->
           <sunburst></sunburst>
 
           <!-- Use this slot to enable edition of props values -->
-          <Editor slot="control" slot-scope="scope" v-bind="scope">
+          <Editor
+            slot="control"
+            slot-scope="scope"
+            v-bind="scope"
+          >
           </Editor>
 
         </component-fixture>
       </div>
-      <div class="tab-pane fade" id="tree-tab" role="tabpanel" aria-labelledby="profile-tab">
+      <div
+        class="tab-pane fade"
+        id="tree-tab"
+        role="tabpanel"
+        aria-labelledby="profile-tab"
+      >
         <component-fixture>
 
           <!-- Use the default slot to create the component under test -->
           <tree></tree>
 
           <!-- Use this slot to enable edition of props values -->
-          <Editor slot="control" slot-scope="scope" v-bind="scope">
+          <Editor
+            slot="control"
+            slot-scope="scope"
+            v-bind="scope"
+          >
           </Editor>
 
         </component-fixture>
       </div>
 
-      <div class="tab-pane fade" id="hello-world-tab" role="tabpanel" aria-labelledby="profile-tab">
+      <div
+        class="tab-pane fade"
+        id="hello-world-tab"
+        role="tabpanel"
+        aria-labelledby="profile-tab"
+      >
         <component-fixture>
 
           <!-- Use the default slot to create the component under test -->
           <HelloWorld></HelloWorld>
 
           <!-- Use this slot to enable edition of props values -->
-          <Editor slot="control" slot-scope="scope" v-bind="scope">
+          <Editor
+            slot="control"
+            slot-scope="scope"
+            v-bind="scope"
+          >
+          </Editor>
+
+        </component-fixture>
+      </div>
+
+      <div
+        class="tab-pane fade"
+        id="gauge"
+        role="tabpanel"
+        aria-labelledby="profile-tab"
+      >
+        <component-fixture>
+
+          <!-- Use the default slot to create the component under test -->
+          <VueSvgGauge></VueSvgGauge>
+
+          <!-- Use this slot to enable edition of props values -->
+          <Editor
+            slot="control"
+            slot-scope="scope"
+            v-bind="scope"
+          >
           </Editor>
 
         </component-fixture>
--- example/main.js
+++ example/main.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
 require("bootstrap");
+import VueSvgGauge from 'vue-svg-gauge'
+
+Vue.use(VueSvgGauge);
 
 Vue.config.productionTip = false;
 
