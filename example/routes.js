321c27063fdd6575345d4e2ac85e83d0199b6ff4
David <desmaisons_david@hotmail.com>
2019-03-30 21:10:22 -0300

    Update routes

--- example/routes.js
+++ example/routes.js
@@ -3,8 +3,9 @@ const requireContext = require.context("./examples/", false, /\.vue$/);
 const componentRoutes = requireContext.keys().map(key => {
   const component = requireContext(key).default;
   const { meta } = component;
+  const path = key.replace(/\.vue$/, "").replace(/^\./, "");
   return {
-    path: `/${key}`,
+    path,
     component,
     meta
   }
