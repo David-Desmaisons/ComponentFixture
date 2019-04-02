4c0f36c7a978a939713bd10a6b39ddfd2e0c7145
David <desmaisons_david@hotmail.com>
2019-04-02 13:13:41 -0300

    Creating component for header

---
+++ src/components/FixtureFeader.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="component__segment">
+    <div class="alert alert-primary name">{{componentName}}</div>
+
+    <div class="btn-group" role="group" aria-label="Basic example">
+      <button
+        v-for="method in methods"
+        :key="method.name"
+        @click="method.execute"
+        type="button"
+        class="btn btn-secondary"
+      >{{method.name}}</button>
+    </div>
+
+    <button class="btn btn-primary" @click="update" v-tooltip.left="'Force component update'">
+      <i class="fa fa-refresh" aria-hidden="true"></i>
+    </button>
+
+    <div class="props-switch">
+      <switch-component :value="showEditor" @input="input"/>
+      <span>Show Props</span>
+    </div>
+  </div>
+</template>
+<script>
+import switchComponent from "./switch";
+
+const props = {
+  componentName: {
+    required: true,
+    type: String
+  },
+  methods: {
+    required: true,
+    type: Array
+  },
+  update: {
+    required: true,
+    type: Function
+  },
+  showEditor: {
+    required: true,
+    type: Boolean
+  }
+};
+
+export default {
+  model: {
+    event: "input",
+    prop: "showEditor"
+  },
+  props,
+  components: {
+    switchComponent
+  },
+  name: "FixtureFeader",
+  methods: {
+    input(evt) {
+      this.$emit("input", evt);
+    }
+  }
+};
+</script>
+<style lang="less" scoped>
+.component__segment {
+  display: flex;
+  padding: 10px;
+  box-shadow: 0 1px 5px rgba(0, 0, 0, 0.3);
+  border-radius: 5px 5px 0 0;
+  padding-left: 30px;
+  padding-right: 30px;
+  overflow-x: auto;
+
+  div {
+    margin-right: 10px;
+    margin-left: 10px;
+    margin-bottom: 0;
+  }
+
+  button {
+    margin-top: 5px;
+    height: 40px;
+  }
+
+  .segment {
+    text-align: center;
+    min-width: 125px;
+    text-align: center;
+    color: #2b3e50;
+    background-color: #eeeeee;
+    padding: 8px;
+    font-weight: 500;
+    text-decoration: none;
+    text-transform: uppercase;
+    font-size: 14px;
+
+    &.selected {
+      font-weight: bold;
+      background: #d5dade;
+      color: #2b3e50;
+      box-shadow: inset 0px 3px #2b3e50;
+    }
+
+    &:first-child {
+      border-radius: 2px 0 0 2px;
+    }
+
+    &:last-child {
+      border-radius: 0 2px 2px 0;
+    }
+
+    &:hover {
+      opacity: 0.9;
+    }
+
+    &:active {
+      -webkit-transform: translateY(1px);
+      transform: translateY(1px);
+    }
+  }
+
+  .custom-control.custom-switch {
+    margin-right: 0;
+  }
+
+  .segment-divider {
+    height: 100%;
+    width: 2px;
+    background: #000;
+  }
+
+  .props-switch {
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
--- src/components/Sandbox.vue
+++ src/components/Sandbox.vue
@@ -1,65 +1,22 @@
 <template>
   <div class="root">
     <div class="component__container">
-
       <div class="component__content">
-        <component-fixture
-          :defaults="defaults"
-          ref="fixture"
-        >
+        <component-fixture :defaults="defaults" ref="fixture">
           <!-- Use the default slot to manipulate the component under test -->
           <template v-slot:header="{componentName, methods, update}">
-            <div class="component__segment">
-
-              <div class="alert alert-primary name">
-                {{componentName}}
-              </div>
-
-              <div
-                class="btn-group"
-                role="group"
-                aria-label="Basic example"
-              >
-                <button
-                  v-for="method in methods"
-                  :key="method.name"
-                  @click="method.execute"
-                  type="button"
-                  class="btn btn-secondary"
-                >{{method.name}}</button>
-              </div>
-
-              <button
-                class="btn btn-primary"
-                @click="update"
-                v-tooltip.left="'Force component update'"
-              >
-                <i
-                  class="fa fa-refresh"
-                  aria-hidden="true"
-                ></i>
-              </button>
-
-              <div class="props-switch">
-                <switch-component v-model="showEditor" />
-                <span>Show Props</span>
-              </div>
-            </div>
+            <FixtureFeader v-bind="{componentName, methods, update}" v-model="showEditor"></FixtureFeader>
           </template>
 
           <!-- Use the default slot to create the component under test -->
           <template v-slot:default>
-            <slot />
+            <slot/>
           </template>
 
           <!-- Use this slot to enable edition of props values -->
           <template v-slot:control="scope">
-            <Editor
-              v-bind="scope"
-              v-show="showEditor"
-            />
+            <Editor v-bind="scope" v-show="showEditor"/>
           </template>
-
         </component-fixture>
       </div>
     </div>
@@ -67,8 +24,8 @@
 </template>
 <script>
 import ComponentFixture from "./ComponentFixture";
-import switchComponent from "./switch";
 import Editor from "./Editor";
+import FixtureFeader from "./FixtureFeader";
 import { VTooltip } from "v-tooltip";
 
 export default {
@@ -85,7 +42,7 @@ export default {
   components: {
     ComponentFixture,
     Editor,
-    switchComponent
+    FixtureFeader
   },
   data() {
     return {
@@ -108,78 +65,6 @@ export default {
   border: 1px solid #eee;
   border-radius: 5px;
   overflow: hidden;
-
-  .props-switch {
-    display: flex;
-    align-items: center;
-  }
-}
-.component__segment {
-  display: flex;
-  padding: 10px;
-  box-shadow: 0 1px 5px rgba(0, 0, 0, 0.3);
-  border-radius: 5px 5px 0 0;
-  padding-left: 30px;
-  padding-right: 30px;
-  overflow-x: auto;
-
-  div {
-    margin-right: 10px;
-    margin-left: 10px;
-    margin-bottom: 0;
-  }
-
-  button {
-    margin-top: 5px;
-    height: 40px;
-  }
-
-  .segment {
-    text-align: center;
-    min-width: 125px;
-    text-align: center;
-    color: #2b3e50;
-    background-color: #eeeeee;
-    padding: 8px;
-    font-weight: 500;
-    text-decoration: none;
-    text-transform: uppercase;
-    font-size: 14px;
-
-    &.selected {
-      font-weight: bold;
-      background: #d5dade;
-      color: #2b3e50;
-      box-shadow: inset 0px 3px #2b3e50;
-    }
-
-    &:first-child {
-      border-radius: 2px 0 0 2px;
-    }
-
-    &:last-child {
-      border-radius: 0 2px 2px 0;
-    }
-
-    &:hover {
-      opacity: 0.9;
-    }
-
-    &:active {
-      -webkit-transform: translateY(1px);
-      transform: translateY(1px);
-    }
-  }
-
-  .custom-control.custom-switch {
-    margin-right: 0;
-  }
-
-  .segment-divider {
-    height: 100%;
-    width: 2px;
-    background: #000;
-  }
 }
 
 .main-panel {
