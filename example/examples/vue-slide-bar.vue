36977860f6ad5a4f2c3c9a5d3e5c03f13be66ee5
David <desmaisons_david@hotmail.com>
2019-04-03 15:56:48 -0300

    Adding vue-slider

---
+++ example/examples/vue-slide-bar.vue
@@ -0,0 +1,25 @@
+<template>
+  <sandbox>
+    <VueSlideBar></VueSlideBar>
+  </sandbox>
+</template>
+<script>
+import Sandbox from "@/components/Sandbox";
+import VueSlideBar from "vue-slide-bar";
+
+export default {
+  name: "vue-slide-bar",
+  meta: {
+    display: "Vue slide bar"
+  },
+  components: {
+    Sandbox,
+    VueSlideBar
+  }
+};
+</script>
+<style lang="less">
+.vue-slide-bar-horizontal {
+  width: 100%;
+}
+</style>
