627cea324e310f3fce5cad53079cbe344a8a8156
David <desmaisons_david@hotmail.com>
2019-04-04 12:01:34 -0300

    WIP- tracking events

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -9,37 +9,10 @@
         :metaData="prop.metaData"
       />
     </collaspable>
-    <!-- <div>
-      <div class="card">
-        <div class="card-header expander">
-          <h2 class="mb-0">
-            <button
-              class="btn expander-button"
-              type="button"
-              data-toggle="collapse"
-              data-target="#collapseOne"
-              aria-expanded="true"
-              aria-controls="collapseOne"
-            >Props</button>
-          </h2>
-        </div>
+    <collaspable title="Events">
+    <pre>{{events}}</pre>
+    </collaspable>
 
-        <div
-          id="collapseOne"
-          class="collapse show"
-        >
-          <div class="card-body">
-            <attributeEditor
-              v-for="prop in props"
-              :key="prop.key"
-              :object="attributes"
-              :attribute="prop.key"
-              :metaData="prop.metaData"
-            />
-          </div>
-        </div>
-      </div>
-    </div>-->
   </div>
 </template>
 <script>
@@ -65,6 +38,10 @@ export default {
     propsDefinition: {
       required: true,
       type: Object
+    },
+    events: {
+      required: true,
+      type: Array
     }
   },
 
