54d6b82c7e1fd7326c73c886e10eb825310958d3
David <desmaisons_david@hotmail.com>
2019-04-05 00:51:07 -0300

    cute rate

---
+++ example/examples/vue-cute-rate.vue
@@ -0,0 +1,20 @@
+<template>
+  <sandbox>
+    <VueCuteRate></VueCuteRate>
+  </sandbox>
+</template>
+<script>
+import Sandbox from "@/components/Sandbox";
+import VueCuteRate from "vue-cute-rate";
+
+export default {
+  name: "vue-cute-rate",
+  meta: {
+    display: "Vue cute rate"
+  },
+  components: {
+    Sandbox,
+    VueCuteRate
+  }
+};
+</script>
