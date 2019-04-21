006585e74c79375961ddd3befbe437a7c597c471
David <desmaisons_david@hotmail.com>
2019-04-21 00:57:16 -0300

    Improving compare method

--- src/utils/compare.js
+++ src/utils/compare.js
@@ -2,7 +2,29 @@ function compareAttribute(o1, o2, attr) {
   return o1[attr] === o2[attr];
 }
 
+function compareArray(o1, o2) {
+  if (o1.length != o2.length) {
+    return false;
+  }
+  return o1.every((el1, idx) => compare(el1, o2[idx]));
+}
+
 function compare(o1, o2) {
+  if (o1 == null || o2 == null){
+    return o1 === o2;
+  }
+
+  if (Array.isArray(o1)) {
+    if (!Array.isArray(o2)) {
+      return false;
+    }
+    return compareArray(o1, o2);
+  }
+
+  if (typeof o1 !== 'object') {
+    return o1 === o2;
+  }
+
   const compareKey = key => compareAttribute(o1, o2, key);
   return Object.keys(o1).every(compareKey) && Object.keys(o2).every(compareKey);
 }
--- tests/unit/utils/compare.spec.js
+++ tests/unit/utils/compare.spec.js
@@ -2,12 +2,31 @@ import compare from "@/utils/compare";
 
 describe("compare", () => {
   test.each([
+    [1, 1, true],
+    [null, null, true],
+    [undefined, undefined, true],
+    ["abc", "abc", true],
     [{}, {}, true],
+    [[], [], true],
+    [["a"], ["a"], true],
+    [[1], [1], true],
+    [["a", "b", 56], ["a", "b", 56], true],
+    [[{ a: 78 }], [{ a: 78 }], true],
+    [{ a: 78, b: "a" }, { a: 78, b: "a" }, true],
     [{ a: 78 }, { a: 78 }, true],
     [{ a: "78" }, { a: "78" }, true],
     [{ a: 78 }, {}, false],
     [{ a: 78 }, { a: "78" }, false],
     [{ a: 78 }, { a: 78, b: "a" }, false],
+    [[], ["a"], false],
+    [[], {}, false],
+    [[], { length: 0 }, false],
+    [["a"], ["b"], false],
+    [[1], [2], false],
+    ["abc", "bac", false],
+    [1, 2, false],
+    [undefined, null, false],
+    [null, undefined, false],
   ])(
     "compare for %o and %o should return: %o",
     (arg1, arg2, expected) => {
