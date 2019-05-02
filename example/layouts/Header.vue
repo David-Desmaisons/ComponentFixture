ded52b56b82d8dc050c426ad5bbe7769e0c78daf
Robson Braga de Queiroz <rbq.dev@gmail.com>
2019-05-01 22:31:08 -0300

    update/add(Header.vue): Update markup header and add multiselect components

--- example/layouts/Header.vue
+++ example/layouts/Header.vue
@@ -1,45 +1,168 @@
 <template>
   <header class="app__header">
-    <nav class="navbar fixed-top">
-      <h1 class="navbar-brand mr-auto">
-        <img
-          src="../assets/logo.png"
-          alt=""
-        />
+    <div class="start-items">
+      <h1 class="app__name">
+        <img src="../assets/logo.png" alt>
         Component Fixture
       </h1>
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a
-            class="nav-link"
-            href="https://github.com/David-Desmaisons/ComponentFixture"
-            target="_blank"
-          >
-            <i class="fa fa-github"></i>
-          </a>
-        </li>
-      </ul>
-    </nav>
+
+      <div class="app__components-select">
+        <multiselect
+          v-model="value"
+          :options="options"
+          :close-on-select="true"
+          :show-labels="false"
+          placeholder="Select component"
+          @select="changeComponent"
+        >
+          <template slot="singleLabel" slot-scope="props">
+            <span class="option__placeholder">Current Component:</span>
+            <div class="option__desc">
+              <strong class="option__title">{{ props.option.meta.display }}</strong>
+            </div>
+          </template>
+          <template slot="option" slot-scope="props">
+            <div class="option__desc">
+              <strong class="option__title">{{ props.option.meta.display }}</strong>
+            </div>
+          </template>
+        </multiselect>
+      </div>
+    </div>
+    <div class="end-items">
+      <a
+        class="github-link"
+        href="https://github.com/David-Desmaisons/ComponentFixture"
+        target="_blank"
+      >
+        <i class="fa fa-github"></i>
+      </a>
+    </div>
   </header>
 </template>
 
-<style lang="less" scoped>
-.app__header {
-  .navbar {
-    background: #fff;
-    border-bottom: 1px solid #eaeaea;
-  }
-  .navbar-brand {
-    margin-bottom: 0;
+<script>
+import Multiselect from "vue-multiselect";
 
-    img {
-      max-width: 40px;
+export default {
+  components: {
+    Multiselect
+  },
+  data() {
+    return {
+      value: "",
+      options: this.$router.options.routes.filter(router => router.meta)
+    };
+  },
+  methods: {
+    changeComponent(routerEvent){
+      this.$router.push({ path: routerEvent.path })
+    }
+  },
+  computed: {
+    routes() {
+      return this.$router.options.routes.filter(r => r.meta);
     }
   }
-  .nav-link {
+};
+</script>
+
+
+<style lang="less" scoped>
+.app__header {
+  background: #fff;
+  border-bottom: 1px solid #eaeaea;
+  padding: 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+
+  h1 {
+    padding: 12px 16px;
+    margin: 0;
+    font-size: 16px;
+    font-weight: bold;
+    border-right: 1px solid #ddd;
+    background: #eee;
+
+    img {
+      max-width: 22px;
+    }
+  }
+
+  .github-link {
     color: black;
-    font-size: 32px;
+    font-size: 22px;
     padding: 0;
+  }
+
+  > div {
+    display: flex;
+    align-items: center;
+  }
+
+  .end-items {
+    padding-right: 16px;
+  }
+}
+
+.app__components-select {
+  /deep/ .multiselect__tags {
+    height: 100%;
+    min-height: 46px;
+    min-width: 260px;
+    border-radius: 0;
+    border: 0;
+    border-right: 1px solid #e8e8e8;
+    border-bottom: 1px solid #e8e8e8;
+    padding-top: 0;
+    padding-bottom: 0;
+    padding-left: 12px;
+    display: flex;
+    align-items: center;
+  }
+  /deep/ .multiselect__placeholder,
+  /deep/ .multiselect__single {
+    margin: 0;
+    padding: 0;
+  }
+  /deep/ .multiselect__select,
+  /deep/ .multiselect__select:before,
+  /deep/ input {
+    padding: 0;
+    margin: 0;
+    transform: translateY(-50%);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  /deep/ .multiselect__single {
+    padding-left: 12px;
+    padding-top: 8px;
+  }
+
+  .option__title {
+    text-transform: capitalize;
+  }
+  .option__placeholder {
+    text-transform: uppercase;
+    color: #35495e96;
+    font-size: 9px;
+    font-weight: bold;
+    position: absolute;
+    top: -8px;
+  }
+
+  .multiselect--active {
+    /deep/ input {
+      position: absolute;
+      top: 50%;
+      background: #f4f4f4;
+      width: calc(100% - 24px) !important;
+      border-radius: 2px;
+      padding: 6px 8px;
+    }
   }
 }
 </style>
