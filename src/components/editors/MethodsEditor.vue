a008c05a8a60c75cdeacd80a4899a566f04d086c
David <desmaisons_david@hotmail.com>
2019-05-14 21:12:44 -0300

    Order methods by name

--- src/components/editors/MethodsEditor.vue
+++ src/components/editors/MethodsEditor.vue
@@ -1,11 +1,27 @@
 <template>
   <div>
-    <div v-if="methods.length>0" class="methods" role="group" aria-label="methods">
-      <div v-for="method in methods" :key="method.name" class="methods-button">
-        <button @click="executeMethod(method)" type="button" class="btn btn-primary">{{method.name}}</button>
+    <div
+      v-if="orderedMethods.length>0"
+      class="methods"
+      role="group"
+      aria-label="methods"
+    >
+      <div
+        v-for="method in orderedMethods"
+        :key="method.name"
+        class="methods-button"
+      >
+        <button
+          @click="executeMethod(method)"
+          type="button"
+          class="btn btn-primary"
+        >{{method.name}}</button>
       </div>
     </div>
-    <span class="no-info" v-else>No methods without argument detected.</span>
+    <span
+      class="no-info"
+      v-else
+    >No methods without argument detected.</span>
   </div>
 </template>
 <script>
@@ -36,6 +52,13 @@ export default {
           ? `"${name}" executed without error`
           : `"${name}" returned: ${JSON.stringify(res, null, 2)}`;
       this.$emit("success", message);
+    }
+  },
+  computed: {
+    orderedMethods() {
+      const ordered = [...this.methods];
+      ordered.sort((a, b) => a.name.localeCompare(b.name));
+      return ordered;
     }
   }
 };
