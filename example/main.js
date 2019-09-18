9242f665d4ac6c7e64b5dc68cb6d848443e0c12f
David <desmaisons_david@hotmail.com>
2019-09-18 14:51:44 -0300

    New version

--- example/main.js
+++ example/main.js
@@ -1,8 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
-import VueSvgGauge from 'vue-svg-gauge'
-import VueRouter from "vue-router"
-import routes from "./routes"
+import VueSvgGauge from 'vue-svg-gauge';
+import VueRouter from "vue-router";
+import routes from "./routes";
+import "font-awesome/less/font-awesome.less";
 
 Vue.use(VueSvgGauge);
 Vue.use(VueRouter);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import ComponentFixture from "./components/ComponentFixture.vue";
 import Editor from "./components/Editor.vue";
 import Sandbox from "./components/Sandbox.vue";
-import "font-awesome/less/font-awesome.less";
 
 export { ComponentFixture, Editor, Sandbox };
